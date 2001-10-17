6cf482cb8446cbe19afab8648a6f811d745eea18
jim winstead <jimw@php.net>
2001-10-17 01:00:30 +0000

    more stuff to export

---
+++ fetch/cvsforwarding.php
@@ -0,0 +1,17 @@
+<?php
+
+# token required, since this should only get accessed from php.net mx
+if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
+  die("token not correct.");
+
+// Connect and generate the list from the DB
+if (@mysql_connect("localhost","nobody","")) {
+  if (@mysql_select_db("php3")) {
+    $res = @mysql_query("SELECT cvsuser,email FROM users WHERE cvsuser IS NOT NULL");
+    if ($res) {
+      while ($row = @mysql_fetch_array($res)) {
+        echo "$row[cvsuser]: $row[email]\n";
+      }
+    }
+  }
+}
---
+++ fetch/cvswriters.php
@@ -0,0 +1,18 @@
+<?php
+
+# token required, since this should only get accessed from cvs.php.net
+if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
+  die("token not correct.");
+
+// Connect and generate the list from the DB
+if (@mysql_connect("localhost","nobody","")) {
+  if (@mysql_select_db("php3")) {
+    $res = @mysql_query("SELECT cvsuser FROM users WHERE cvsuser IS NOT NULL AND passwd != ''");
+    if ($res) {
+      while ($row = @mysql_fetch_array($res)) {
+        if ($row[cvsuser] == 'cvsread') continue;
+        echo "$row[cvsuser]\n";
+      }
+    }
+  }
+}
