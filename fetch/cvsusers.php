733d3f6e2a7b5a4153b0d7947522a80645612475
jim winstead <jimw@php.net>
2001-10-17 23:22:25 +0000

    splitting cvs-specific stuff to distinct table.

--- fetch/cvsusers.php
+++ fetch/cvsusers.php
@@ -7,7 +7,7 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 // Connect and generate the list from the DB
 if (@mysql_connect("localhost","nobody","")) {
   if (@mysql_select_db("php3")) {
-    $res = @mysql_query("SELECT cvsuser,name,email FROM users WHERE cvsuser IS NOT NULL AND passwd != ''");
+    $res = @mysql_query("SELECT users_cvs.cvsuser,name,email FROM users LEFT JOIN users_cvs USING (userid) WHERE email != '' AND users_cvs.approved");
     if ($res) {
       while ($row = @mysql_fetch_array($res)) {
         echo "$row[cvsuser]:$row[name]:$row[email]\n";
--- fetch/cvswriters.php
+++ fetch/cvswriters.php
@@ -7,7 +7,7 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 // Connect and generate the list from the DB
 if (@mysql_connect("localhost","nobody","")) {
   if (@mysql_select_db("php3")) {
-    $res = @mysql_query("SELECT cvsuser FROM users WHERE cvsuser IS NOT NULL AND passwd != ''");
+    $res = @mysql_query("SELECT cvsuser FROM users_cvs WHERE approved");
     if ($res) {
       while ($row = @mysql_fetch_array($res)) {
         if ($row[cvsuser] == 'cvsread') continue;
