02efae526354b30c8a39123e6eb5c6d5674ce834
jim winstead <jimw@php.net>
2001-10-17 03:03:56 +0000

    skip entries without email addresses

--- fetch/cvsforwarding.php
+++ fetch/cvsforwarding.php
@@ -7,7 +7,7 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 // Connect and generate the list from the DB
 if (@mysql_connect("localhost","nobody","")) {
   if (@mysql_select_db("php3")) {
-    $res = @mysql_query("SELECT cvsuser,email FROM users WHERE cvsuser IS NOT NULL");
+    $res = @mysql_query("SELECT cvsuser,email FROM users WHERE cvsuser IS NOT NULL AND email != ''");
     if ($res) {
       while ($row = @mysql_fetch_array($res)) {
         echo "$row[cvsuser]: $row[email]\n";
