57de15fd7810f8ea3feaac4e114f7855a8be0ed1
jim winstead <jimw@php.net>
2001-10-18 02:25:14 +0000

    email may be null, but not empty

--- fetch/cvsforwarding.php
+++ fetch/cvsforwarding.php
@@ -7,7 +7,7 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 // Connect and generate the list from the DB
 if (@mysql_connect("localhost","nobody","")) {
   if (@mysql_select_db("php3")) {
-    $res = @mysql_query("SELECT cvsuser,email FROM users LEFT JOIN users_cvs USING (userid) WHERE email != '' AND approved");
+    $res = @mysql_query("SELECT cvsuser,email FROM users LEFT JOIN users_cvs USING (userid) WHERE email IS NOT NULL AND approved");
     if ($res) {
       while ($row = @mysql_fetch_array($res)) {
         echo "$row[cvsuser]: $row[email];\n";
