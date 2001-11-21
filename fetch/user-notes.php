93a0c6501725d31834de76dc860b14e5554c6653
jim winstead <jimw@php.net>
2001-11-21 01:31:50 +0000

    get rid of some silly code, remove last of 'removed' stuff

--- fetch/user-notes.php
+++ fetch/user-notes.php
@@ -11,14 +11,12 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 
 $query  = "SELECT DISTINCT id,note.sect,user,note,UNIX_TIMESTAMP(ts) AS ts,";
 $query .= "IF(votes=0, 10, rating/votes) AS rate";
-$query .= " FROM note WHERE NOT removed";
+$query .= " FROM note";
 $query .= " ORDER BY sect,rate DESC,ts";
 
 $res = @mysql_query($query) or exit;
 
-while ($row = mysql_fetch_array($res)) {
-  # get rid of the ordered fields, we just want the named ones.
-  for ($i = count($row) / 2; $i >= 0; $i--) unset($row[$i]);
+while ($row = mysql_fetch_array($res,MYSQL_ASSOC)) {
   echo "$row[id]|$row[sect]|$row[rate]|$row[ts]|$row[user]|",
        base64_encode(gzcompress($row[note],3)),"\n";
 }
