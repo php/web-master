768b6ea4c101c0751dde12353b7e68956060fbd3
jim winstead <jimw@php.net>
2002-03-03 18:51:04 +0000

    generate php code for list of countries

---
+++ fetch/countries.php
@@ -0,0 +1,20 @@
+<?php
+
+# token required, since this should only get accessed from rsync.php.net
+if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
+  die("token not correct.");
+
+// Connect and generate the list from the DB
+if (@mysql_connect("localhost","nobody","")) {
+  if (@mysql_select_db("php3")) {
+    $res = @mysql_query("SELECT * FROM country ORDER BY name");
+    if ($res) {
+      echo "<?php\n\$COUNTRIES = array(\n";
+      while ($row = @mysql_fetch_array($res)) {
+        echo "'$row[id]' => '$row[name]',\n";
+      }
+      echo ");\n";
+      echo "?>\n";
+    }
+  }
+}
