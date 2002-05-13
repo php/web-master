a52a7993094a9ee669d348229da063404abd38aa
jim winstead <jimw@php.net>
2002-05-13 02:41:25 +0000

    some basic stats from the database

---
+++ fetch/stats.php
@@ -0,0 +1,14 @@
+<?php
+require 'functions.inc';
+
+# token required, since this should only get accessed from rsync.php.net
+if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
+  die("token not correct.");
+
+// Connect and generate the list from the DB
+if (@mysql_pconnect("localhost","nobody","")) {
+   echo 'notes ', mysql_get_one("SELECT COUNT(*) FROM php3.note"), "\n";
+   echo 'hosts ', mysql_get_one("SELECT COUNT(*) FROM phphosts.hosts"), "\n";
+   echo 'bugs ', mysql_get_one("SELECT COUNT(*) FROM php3.bugdb"), "\n";
+   echo 'openbugs ', mysql_get_one("SELECT COUNT(*) FROM php3.bugdb WHERE status='Open' OR status='Assigned' OR status='Analyzed' OR status='Critical'"), "\n";
+}
