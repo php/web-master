73641d29ff2564d64f00d8cc4205bf089ebef803
jim winstead <jimw@php.net>
2002-06-02 05:08:08 +0000

    list mirrors that aren't current as not active

--- fetch/mirrors.php
+++ fetch/mirrors.php
@@ -7,13 +7,13 @@ if (!isset($token) || md5($token) != "19a3ec370affe2d899755f005e5cd90e")
 // Connect and generate the list from the DB
 if (@mysql_pconnect("localhost","nobody","")) {
   if (@mysql_select_db("php3")) {
-    $res = @mysql_query("SELECT mirrors.*,country.name AS cname FROM mirrors LEFT JOIN country ON mirrors.cc = country.id ORDER BY country.name,hostname");
+    $res = @mysql_query("SELECT mirrors.*,country.name AS cname,(DATE_SUB(NOW(),INTERVAL 3 DAY) < mirrors.lastupdated) AS current FROM mirrors LEFT JOIN country ON mirrors.cc = country.id ORDER BY country.name,hostname");
     if ($res) {
       echo "<?php\n\$MIRRORS = array(\n";
       while ($row = @mysql_fetch_array($res)) {
         if (!strstr("http:",$row[hostname])) { $row[hostname]="http://$row[hostname]/"; }
         // Set inactive mirrors to type 2 so they won't show up in the drop-down.
-        if (!$row["active"]) { $row["mirrortype"] = 2; }
+        if (!$row["active"] || !$row["current"]) { $row["mirrortype"] = 2; }
         echo "  \"$row[hostname]\" => array(\"$row[cc]\",\"$row[providername]\",$row[has_stats],\"$row[providerurl]\",$row[mirrortype],$row[has_search],\"$row[lang]\"),\n";
       }
       echo '  0 => array("xx", "Unknown", 0, "/", 2, 0, "en")', "\n";
