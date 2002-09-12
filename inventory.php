ae9b6aeee1c041419568a3794a446a52fbe0b2b5
jim winstead <jimw@php.net>
2002-09-12 15:13:02 +0000

    update, now that everything is moved off the synacor machines

--- inventory.php
+++ inventory.php
@@ -9,12 +9,6 @@ what services they provide.</p>
 <p>Note regarding FreeBSD machines: Upgrades should be 
 performed according to <a href="fbsd_upgrade.txt">this guide</a>.</p>
 
-<h2>chek2.php.net (208.210.50.161)</h2>
-
-<p><b>machine:</b> dual p3/650, 512MB RAM, 40.9GB HD, redhat 6.1</p>
-
-<p><b>aliases:</b>none</p>
-
 <h2>pair1.php.net (216.92.131.4)</h2>
 
 <p><b>machine:</b> dual p3/1000 xeon, 512MB RAM, 27GB HD, freebsd 4.6</p>
@@ -28,6 +22,8 @@ level by pair, due to the recent snmp security advisories).</p>
 
 <p><b>machine:</b> p3/566, 128MB RAM, 27GB HD, freebsd 4.6</p>
 
+<p><b>aliases:</b> php.net mx</p>
+
 <p><b>notes:</b> this machine maintains a copy of ~ezmlm from pair1, so it can
 take over the mailing lists if pair1 ever fails.</p>
 
@@ -39,27 +35,9 @@ take over the mailing lists if pair1 ever fails.</p>
 bandwidth limitations, and we need to make sure monitoring is in place before
 resuming use of the machine to make sure we don't exceed those limits.</p>
 
-<p><b>aliases:</b> qa.php.net</p>
-
-<p><b>notes:</b> taking on the database-backed web functions from synacor1.</p>
+<p><b>aliases:</b> qa.php.net, bugs.php.net, gtk.php.net, master.php.net</p>
 
 <p>limited to 500GB traffic monthly.</p>
-
-<h2>synacor1.php.net (208.210.50.162)</h2>
-
-<p><b>machine:</b> dual p3/450, 256MB RAM, 10GB HD (8GB RAID), redhat 7.2</p>
-
-<p><b>aliases:</b> php.net mx, bugs.php.net, gtk.php.net, hosts.php.net, master.php.net</p>
-
-<h2>va1.php.net (198.186.203.51)</h2>
-
-<p><b>machine:</b> dual p3/650, 1 GB RAM, 130GB HD, debian-stable</p>
-
-<p><b>aliases:</b> rsync.php.net, snaps.php.net</p>
-
-<p><b>notes:</b> this machine generates the online manuals. daily backups of
-the cvs repository and the rsync modules are done. cricket also runs on this
-machine to track the usage of all of the machines.</p>
 
 <h2>rs1.php.net (64.246.30.37)</h2>
 
@@ -71,6 +49,16 @@ machine to track the usage of all of the machines.</p>
 
 <p><b>stats:</b> MRTG stats (traffic, load avg, cpu usage, etc) are 
 available <a href="http://www.php.net/~imajes/mrtg/">here</a>.</p>
+
+<h2>va1.php.net (198.186.203.51)</h2>
+
+<p><b>machine:</b> dual p3/650, 1 GB RAM, 130GB HD, debian-stable</p>
+
+<p><b>aliases:</b> rsync.php.net, snaps.php.net</p>
+
+<p><b>notes:</b> this machine generates the online manuals. daily backups of
+the cvs repository and the rsync modules are done. cricket also runs on this
+machine to track the usage of all of the machines.</p>
 
 <p>originally compiled by jim winstead, september 2001</p>
 <p>last update: $Date$</p>
