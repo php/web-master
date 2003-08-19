6b2652d05c2316d5fadcc17cc9f2a5ec203c7c91
Sascha Schumann <sas@php.net>
2003-08-19 19:33:29 +0000

    add some, fix some

--- inventory.php
+++ inventory.php
@@ -11,21 +11,25 @@ performed according to <a href="fbsd_upgrade.txt">this guide</a>.</p>
 
 <h2>pair1.php.net (216.92.131.4)</h2>
 
-<p><b>machine:</b> dual p3/1000 xeon, 512MB RAM, 27GB HD, freebsd 4.6</p>
+<p><b>machine:</b> dual p3/1000 xeon, 512MB RAM, 27GB HD, freebsd 4.8</p>
 
-<p><b>aliases:</b> lists.php.net, news.php.net, cvs.php.net, cvsup.php.net, pear.php.net</p>
-
-<p><b>notes:</b> snmp is currently blocked on this machine (at the network
-level by pair, due to the recent snmp security advisories).</p>
+<p><b>aliases:</b> lists.php.net, news.php.net, pear.php.net</p>
 
 <h2>pair2.php.net (216.92.131.5)</h2>
 
-<p><b>machine:</b> p3/566, 128MB RAM, 27GB HD, freebsd 4.6</p>
+<p><b>machine:</b> p3/566, 128MB RAM, 27GB HD, freebsd 4.8</p>
 
 <p><b>aliases:</b> php.net mx</p>
 
 <p><b>notes:</b> this machine maintains a copy of ~ezmlm from pair1, so it can
 take over the mailing lists if pair1 ever fails.</p>
+
+<h2>pair11.php.net (216.92.131.65)</h2>
+
+<p><b>machine:</b> dual p3/1000 xeon, 512MB RAM, 70GB HD, freebsd 4.8</p>
+
+<p><b>aliases:</b> cvs, cvsup, lxr</p>
+
 
 <h2>rack1.php.net (209.61.157.217)</h2>
 
@@ -68,7 +72,12 @@ available <a href="http://www.php.net/~imajes/mrtg/">here</a>.</p>
 this box. This allows for absolute failover redundancy, and the possibilty of internal linkage. This box is quite a monster,
  so having failover allows us the possibility to experiment with various distributed setups.</p>
 
+<h2>rn2.php.net (67.72.78.18)</h2>
 
+<p><b>machine:</b> (same as rn1) P3-1133, 1.2GB RAM, 165GB HD</p>
+
+<p><b>aliases:</b> bonsai</p>
+ 
 <!-- this machine generates the online manuals. daily backups of
 the cvs repository and the rsync modules are done. cricket also runs on this
 machine to track the usage of all of the machines.</p> -->
