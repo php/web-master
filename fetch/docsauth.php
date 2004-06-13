47f79e29ae366f73fe18b2ef0c33704557e92d54
Gabor Hojtsy <goba@php.net>
2004-06-13 12:56:44 +0000

    fix register_globals vulnerability and tabs to spaces

--- fetch/docsauth.php
+++ fetch/docsauth.php
@@ -3,22 +3,25 @@
 
 require_once 'cvs-auth.inc';
 
-$user = null;
-$pw = null;
+$user = $pw = null;
 
+// Protect from register_globals vulnerability
+if (isset($_GET['_SERVER']) || isset($_POST['_SERVER']) || isset($_COOKIE['_SERVER'])) {
+    die("ERR: Permission denied.");
+}
 if ($_SERVER['REMOTE_ADDR'] != '81.57.253.132' &&
-	md5($_GET['token']) != '97669cf8c9a1449eb2099ea6147d125697669cf8c9a1449eb2099ea6147d1256') {
-	die ("ERR: Permission denied.");
+    md5($_GET['token']) != '97669cf8c9a1449eb2099ea6147d125697669cf8c9a1449eb2099ea6147d1256') {
+    die("ERR: Permission denied.");
 }
 
 if (isset($_GET['credential'])) {
-	list($user, $pw) = explode(":", base64_decode($_GET['credential']));
+    list($user, $pw) = explode(":", base64_decode($_GET['credential']));
 } else {
-	die ("ERR: No credential passed.");
+    die("ERR: No credential passed.");
 }
 
 if (verify_password($user, $pw)) {
-	die ("OK");
+    die("OK");
 } else {
-	die ("ERR: Login failed.");
+    die("ERR: Login failed.");
 }
