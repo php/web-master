4c5b6abe2709731c76a92a69238130452b9bbc8a
Jacques Marneweck <jacques@php.net>
2004-09-11 16:05:32 +0000

    Update token for doc.php.net

--- fetch/docsauth.php
+++ fetch/docsauth.php
@@ -9,8 +9,8 @@ $user = $pw = null;
 if (isset($_GET['_SERVER']) || isset($_POST['_SERVER']) || isset($_COOKIE['_SERVER'])) {
     die("ERR: Permission denied.");
 }
-if ($_SERVER['REMOTE_ADDR'] != '81.57.253.132' &&
-    md5($_GET['token']) != '97669cf8c9a1449eb2099ea6147d125697669cf8c9a1449eb2099ea6147d1256') {
+if ($_SERVER['REMOTE_ADDR'] != '65.75.184.90' &&
+    md5($_GET['token']) != 'e6bb2b8f864bcef96c4608d49aa76596') {
     die("ERR: Permission denied.");
 }
 
