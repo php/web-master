9fb7636a422a2b08f0c3cfa230cf7c2ce9bc2f08
Philip Olson <philip@php.net>
2007-05-14 23:57:01 +0000

    Allow mirror admins to view phpinfo(), is_mirror_site_admin() taken from ./mirrors.php

---
+++ manage/pinfo.php
@@ -0,0 +1,26 @@
+<?php
+require 'login.inc';
+require 'functions.inc';
+
+if (!is_mirror_site_admin($user)) {
+    warn("Sorry, you are not allowed to view this web page");
+    exit;
+}
+
+phpinfo();
+
+function is_mirror_site_admin($user) {
+    if (in_array(
+            $user,
+            array(
+                "jimw", "rasmus", "andrei", "zeev", "andi", "sas", "thies",
+                "rubys", "ssb", "imajes", "goba", "derick", "cortesi", "wez", 
+                "bjori", "philip"
+            )
+        )
+    ) {
+        return TRUE;
+    } else { 
+        return FALSE; 
+    }
+}
