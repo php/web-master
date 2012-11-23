532a6f0200dd439a1c61bc0955531d3a51e13aab
Sherif Ramadan <googleguy@php.net>
2012-11-22 21:37:05 -0500

    Added myself to view phpinfo.

--- manage/pinfo.php
+++ manage/pinfo.php
@@ -14,7 +14,7 @@ function is_mirror_site_admin($user) {
             array(
                 "jimw", "rasmus", "andrei", "zeev", "andi", "sas", "thies",
                 "rubys", "ssb", "imajes", "goba", "derick", "cortesi", "wez", 
-                "bjori", "philip", "danbrown"
+                "bjori", "philip", "danbrown", "googleguy",
             )
         )
     ) {
