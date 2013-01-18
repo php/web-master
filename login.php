24cdb723f439eda5db0f76f1aa9c638cccd56d03
Daniel P. Brown <danbrown@php.net>
2013-01-18 14:57:30 -0500

    Making logouts easier for the first time in a dozen years.

--- login.php
+++ login.php
@@ -1,6 +1,14 @@
 <?php
 // $Id$
 
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+        session_start();
+        session_destroy();
+        echo 'You have successfully logged out.  Redirecting....'.PHP_EOL;
+        echo '<meta http-equiv="refresh" content="5;http://php.net/"/>'.PHP_EOL;
+        exit(0);
+}
+
 require 'login.inc';
 
 head();
