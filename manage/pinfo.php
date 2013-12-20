605a7f2e8d4fe6ad6fe178fe77cf81c0ef79e465
Sherif Ramadan <googleguy@php.net>
2013-12-20 05:08:38 -0500

    Fix wrong global variable used due to bjori's commit from c659097ec0d0a08327b80c1b32ac0797f35ba5dd
    Admin users can't be identified otherwise.

--- manage/pinfo.php
+++ manage/pinfo.php
@@ -1,7 +1,7 @@
 <?php
 require '../include/login.inc';
 
-if (!is_mirror_site_admin($user)) {
+if (!is_mirror_site_admin($cuser)) {
     warn("Sorry, you are not allowed to view this web page");
     exit;
 }
