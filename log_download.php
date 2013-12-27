38659d3ec16a556cb7078601865f09685879d3c9
Hannes Magnusson <bjori@mongodb.com>
2013-12-26 17:19:14 -0800

    We don't log downloads anymore (years?) fixes bug #54377

--- log_download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$log = @fopen("/local/log/download_log", "a");
-if ($log) {
-    $log_line = sprintf("%s %s %s %s %s", date("H:i:s d-M-Y"), $user_referer, $download_file, gethostbyaddr($user_ip), $mirror);
-    fputs($log, "$log_line\n");
-    fclose($log);
-}
-?>
