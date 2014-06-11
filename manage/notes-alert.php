bdf97a6fd4cc435707bb5c6797842ca817e41f9a
kovacs.ferenc <kovacs.ferenc@ustream.tv>
2014-06-11 13:45:39 +0200

    $user was renamed to $cuser in include/login.inc in c659097ec0d0a08327b80c1b32ac0797f35ba5dd also fix the column names

--- manage/notes-alert.php
+++ manage/notes-alert.php
@@ -7,7 +7,7 @@ head();
 	
 switch ($alert_action) {
 	case "add_alert" :
-		$sql = "INSERT INTO alerts VALUES ('$user', '$sect', NOW())";
+		$sql = "INSERT INTO alerts VALUES ('$cuser', '$sect', NOW())";
 		if (has_alert($user, $sect))
 			echo "<b>You already have an alert for this page</b><br />\n";
 		else
@@ -17,7 +17,7 @@ switch ($alert_action) {
 				echo "Unknown error while adding alert<br />\n";
 		break;
 	case "del_alert" :
-		$sql = "delete from alerts where admin='$user' and page='$sect'";
+		$sql = "delete from alerts where user='$cuser' and sect='$sect'";
 		if (has_alert($user, $sect))
 			if(do_alert_action($sql))
 				echo "<b>Alert for page \"$sect\", deleted from your list</b></ br>";
