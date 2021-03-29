bce8ee3ca48fcc9b7220c16e1618cc9c5259cf86
Nikita Popov <nikita.ppv@gmail.com>
2021-03-29 18:25:59 +0200

    Add webhook to auto-close bugs

--- github-webhook.php
+++ github-webhook.php
@@ -1,4 +1,7 @@
 <?php
+
+const DRY_RUN = false;
+
 function verify_signature($requestBody) {
     if (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])){
         $sig = 'sha1=' . hash_hmac('sha1', $requestBody, getenv('GITHUB_SECRET'));
@@ -37,9 +40,70 @@ function send_mail($to, $subject, $message, $headers) {
     printf("Sending mail...\nTo: %s\nSubject: %s\nMessage:\n%s\nHeaders:\n%s",
         $to, $subject, $message, $headers);
 
-    mail($to, '=?utf-8?B?'.base64_encode($subject).'?=', $message, $headers, "-fnoreply@php.net");
+    if (!DRY_RUN) {
+        $subject = '=?utf-8?B?'.base64_encode($subject).'?=';
+        mail($to, $subject, $message, $headers, "-fnoreply@php.net");
+    }
 }
 
+function handle_commit($commit) {
+    $message = $commit->message;
+    $author = $commit->author->username;
+    $committer = $commit->committer->username;
+    $url = $commit->url;
+
+    if (!preg_match_all('/^Fix(?:ed)? (?:bug )?\#([0-9]+)/m', $message, $matches)) {
+        return;
+    }
+    $bugIds = $matches[1];
+
+    if ($author === $committer) {
+        $blame = $author;
+    } else {
+        $blame = "$author (author) and $committer (committer)";
+    }
+
+    $newLinePos = strpos($message, "\n");
+    $firstLine = $newLinePos !== false ? substr($message, 0, $newLinePos) : $message;
+
+    $comment = <<<MSG
+Automatic comment on behalf of $blame
+Revision: $url
+Log: $firstLine
+MSG;
+
+    foreach ($bugIds as $bugId) {
+        $postData = [
+            'user' => 'git',
+            'id' => (int) $bugId,
+            'ncomment' => $comment,
+            'status' => 'Closed',
+            'MAGIC_COOKIE' => getenv('BUGS_MAGIC_COOKIE'),
+        ];
+        $postData = http_build_query($postData, '', '&');
+
+        $curlOpts =  [
+            CURLOPT_URL => 'https://bugs.php.net/rpc.php',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_CONNECTTIMEOUT => 5,
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_POSTFIELDS => $postData,
+        ];
+
+        echo "Closing bug #$bugId\n";
+        if (!DRY_RUN) {
+            $ch = curl_init();
+            curl_setopt_array($ch, $curlOpts);
+            $result = curl_exec($ch);
+            if ($result === false) {
+                echo "Error: ", curl_error($ch), "\n";
+            } else {
+                echo "Response: $result\n";
+            }
+        }
+    }
+}
 
 $CONFIG = [
     'repos' => [
@@ -50,23 +114,28 @@ $CONFIG = [
     ],
 ];
 
-$body = file_get_contents("php://input");
+if (DRY_RUN) {
+    $body = file_get_contents("php://stdin");
+    $event = $argv[1];
+} else {
+    $body = file_get_contents("php://input");
+    $event = $_SERVER['HTTP_X_GITHUB_EVENT'];
 
-if (!verify_signature($body)) {
-    header('HTTP/1.1 403 Forbidden');
-    exit;
+    if (!verify_signature($body)) {
+        header('HTTP/1.1 403 Forbidden');
+        exit;
+    }
 }
 
 $payload = json_decode($body);
-$action = $payload->action;
 $repoName = $payload->repository->name;
 
-$event = $_SERVER['HTTP_X_GITHUB_EVENT'];
 switch ($event) {
     case 'ping':
         break;
     case 'pull_request':
     case 'issues':
+        $action = $payload->action;
         $issue = $event == 'issues' ? $payload->issue : $payload->pull_request;
         $htmlUrl = $issue->html_url;
 
@@ -108,6 +177,7 @@ switch ($event) {
 
     case 'pull_request_review_comment':
     case 'issue_comment':
+        $action = $payload->action;
         $issue = $event == 'issue_comment' ? $payload->issue : $payload->pull_request;
         $htmlUrl = $issue->html_url;
 
@@ -131,6 +201,16 @@ switch ($event) {
 
         $headers = "From: noreply@php.net\r\nContent-Type: text/plain; charset=utf-8\r\n";
         send_mail($to, $subject, $message, $headers);
+        break;
+
+    case 'push':
+        if ($payload->ref !== 'refs/heads/master') {
+            echo "Skipping push to non-master";
+            break;
+        }
+        foreach ($payload->commits as $commit) {
+            handle_commit($commit);
+        }
         break;
 
     default:
