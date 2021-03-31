cfbbcff7222b332e831b146e64786f00d96430b2
Nikita Popov <nikita.ppv@gmail.com>
2021-03-31 15:33:09 +0200

    Send mail for ref create/delete/force

--- github-webhook.php
+++ github-webhook.php
@@ -2,7 +2,9 @@
 
 const DRY_RUN = false;
 
-require __DIR__ . '/include/mailer.php';
+if (!DRY_RUN) {
+    require __DIR__ . '/include/mailer.php';
+}
 
 function verify_signature($requestBody) {
     if (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])){
@@ -38,17 +40,17 @@ function prep_title($issue, $repoName) {
     return $subject;
 }
 
-function send_mail($to, $subject, $message) {
-    printf("Sending mail...\nTo: %s\nSubject: %s\nMessage:\n%s",
-        $to, $subject, $message);
+function send_mail($to, $subject, $message, $from = "noreply@php.net", $from_name = "") {
+    printf("Sending mail...\nTo: %s\nFrom: %s <%s>\nSubject: %s\nMessage:\n%s",
+        $to, $from_name, $from, $subject, $message);
 
     if (!DRY_RUN) {
         $subject = '=?utf-8?B?'.base64_encode($subject).'?=';
-        mailer($to, $subject, $message);
+        mailer($to, $subject, $message, $from, $from_name);
     }
 }
 
-function handle_commit($commit) {
+function handle_bug_close($commit) {
     $message = $commit->message;
     $author = $commit->author->username;
     $committer = $commit->committer->username;
@@ -96,6 +98,121 @@ MSG;
             echo "Response: $result\n";
         }
     }
+}
+
+function get_commit_mailing_list($repoName) {
+    if ($repoName === 'playground') {
+        return 'nikic@php.net';
+    }
+    // Only enable playground for now.
+    return null;
+
+    if ($repoName === 'php-src' || $repoName === 'karma') {
+        return 'php-cvs@lists.php.net';
+    } else if ($repoName === 'php-langspec') {
+        return 'standards-vcs@lists.php.net';
+    } else if ($repoName === 'phpruntests' || $repoName === 'pftt2' || $repoName === 'web-qa') {
+        return 'php-qa@lists.php.net';
+    } else if ($repoName === 'systems') {
+        return 'systems@php.net';
+    } else if ($repoName === 'php-gtk-src') {
+        return 'php-gtk-cvs@lists.php.net';
+    } else if ($repoName === 'presentations' || $repoName === 'web-pres2') {
+        return 'pres@lists.php.net';
+    } else if ($repoName === 'doc-base' || $repoName === 'doc-en'
+        || $repoName === 'phd' || $repoName === 'web-doc-editor') {
+        return 'doc-cvs@lists.php.net';
+    } else if ($repoName === 'web-doc') {
+        return 'doc-web@lists.php.net';
+    } else if ($repoName === 'web-pecl') {
+        return 'pecl-cvs@lists.php.net';
+    } else if (strpos($repoName, 'web-') === 0) {
+        return 'php-webmaster@lists.php.net';
+    } else if (strpos($repoName, 'pecl-') === 0) {
+        return 'pecl-cvs@lists.php.net';
+    } else if (strpos($repoName, 'doc-') === 0 && $repoName !== 'doc-gtk') {
+        return str_replace('-', '_', $repoName) . '@lists.php.net';
+    } else {
+        return null;
+    }
+}
+
+function handle_ref_change_mail($mailingList, $payload) {
+    $repoName = $payload->repository->name;
+    $ref = $payload->ref;
+    $before = $payload->before;
+    $after = $payload->after;
+    $compare = $payload->compare;
+    $pusherName = $payload->pusher->name;
+
+    if (!preg_match('/^refs\/(.+)\/(.+)$/', $ref, $matches)) {
+        echo "Unexpected ref format: $ref";
+        return;
+    }
+
+    $refKind = $matches[1];
+    $refName = $matches[2];
+
+    if ($refKind === 'heads') {
+        $what = "branch $refName";
+    } else if ($refKind === 'tags') {
+        $what = "tag $refName";
+    } else {
+        $what = "unknown ref $ref";
+    }
+
+    if ($payload->created) {
+        $action = "created";
+    } else if ($payload->deleted) {
+        $action = "deleted";
+    } else if ($payload->forced) {
+        $action = "force pushed";
+    } else {
+        $action = "performed unknown action on";
+    }
+
+    $subject = "[$repoName] $action $what";
+
+    $message = ucfirst($action) . " $what in repository $repoName.\n\n";
+    $message .= "Pusher: $pusherName\n";
+    if ($action !== 'created') {
+        $message .= "Before: https://github.com/php/$repoName/commit/$before\n";
+    }
+    if ($action !== 'deleted') {
+        $message .= "After: https://github.com/php/$repoName/commit/$after\n";
+    }
+    $message .= "Compare: $compare\n";
+    $message .= "Tree: https://github.com/php/$repoName/tree/$refName\n";
+    if ($refKind === 'tags') {
+        $message .= "Tag: https://github.com/php/$repoName/releases/tag/$refName\n";
+    }
+
+    send_mail($mailingList, $subject, $message, 'noreply@php.net', $pusherName);
+}
+
+function handle_push_mail($payload) {
+    $repoName = $payload->repository->name;
+    $ref = $payload->ref;
+    $mailingList = get_commit_mailing_list($repoName);
+    if ($mailingList === null) {
+        echo "Not sending mail for $repoName (no mailing list)";
+        return;
+    }
+
+    if ($payload->created || $payload->deleted || $payload->forced) {
+        handle_ref_change_mail($mailingList, $payload);
+    }
+
+    // Ignore commits to PHP-x.y branches for now, to avoid duplicate commits on upwards merges.
+    // TODO: Find a better solution to this problem...
+    if ($repoName === 'php-src' && preg_match('/^refs/heads/PHP-\d\.\d$/', $ref)) {
+        echo "Skipping commit mails for push to $payload->ref of $repoName";
+        return;
+    }
+
+    /*foreach ($payload->commits as $commit) {
+
+    }*/
 }
 
 $CONFIG = [
@@ -195,13 +312,14 @@ switch ($event) {
         break;
 
     case 'push':
-        if ($payload->ref !== 'refs/heads/master') {
-            echo "Skipping push to non-master";
-            break;
+        if ($payload->ref === 'refs/heads/master') {
+            // Only close bugs for pushes to master.
+            foreach ($payload->commits as $commit) {
+                handle_bug_close($commit);
+            }
         }
-        foreach ($payload->commits as $commit) {
-            handle_commit($commit);
-        }
+
+        handle_push_mail($payload);
         break;
 
     default:
