192af434abb2b2512c28c63f4950aced0a879bfd
Niklas Keller <me@kelunik.com>
2021-04-11 21:11:02 +0200

    Centralize DB credentials

--- src/DB.php
+++ src/DB.php
@@ -4,10 +4,35 @@ namespace App;
 
 use PDO;
 
-final class DB extends PDO {
-    public static function connect() {
-        $dbh = new self('mysql:host=localhost;dbname=phpmasterdb', 'nobody', '');
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $dbh;
+final class DB extends PDO
+{
+    public static function connect(): self
+    {
+        $connectionConfig = 'mysql:host=' . self::getHost() . ';dbname=' . self::getDatabase();
+
+        $db = new self($connectionConfig, self::getUser(), self::getPassword());
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+    }
+
+    public static function getHost(): string
+    {
+        return \getenv("DATABASE_HOST") ?: "localhost";
+    }
+
+    public static function getUser(): string
+    {
+        return \getenv("DATABASE_USER") ?: "nobody";
+    }
+
+    public static function getPassword(): string
+    {
+        return \getenv("DATABASE_PASSWORD") ?: "";
+    }
+
+    public static function getDatabase(): string
+    {
+        return \getenv("DATABASE_NAME") ?: "phpmasterdb";
     }
 }
